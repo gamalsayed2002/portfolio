b40033b90a94e5efa02703f1e57bd4c2b1cadbe3
gamal <gamalsayedccss@gmail.com>
2024-03-15 16:47:58 +0200

    fix bug

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ window.addEventListener("scroll", () => {
 });
 
 // end scroller
-// start scroll to up
-let scrollW = document.querySelector(".scroll-to-up");
-window.onscroll = function () {};
+
 
 // start nav
 let menu = document.querySelector(".menu");
@@ -27,63 +25,39 @@ menu.onclick = function () {
   specail.classList.toggle("js-class");
 };
 
+let progress = document.querySelectorAll(".skill-progress");
+
+
+
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
 window.onscroll = function () {
-  if (this.scrollY > 85) {
+  if (this.scrollY > 100) {
     nav.classList.add("fixed");
   } else {
     nav.classList.remove("fixed");
   }
-  // scroll to up
-  if (this.scrollY > 400) {
-    scrollW.style.opacity = "1";
-  } else {
-    scrollW.style.opacity = "0";
+
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  //Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  //Window ScrollTop
+  let windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop < skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
   }
-
-  // remove active class when click on link
-  let li = document.querySelectorAll(".landing nav ul li");
-  li.forEach((a) => {
-    a.addEventListener("click", (e) => {
-      ul.classList.remove("active");
-      specail.classList.remove("js-class");
-    });
-  });
-
-};
-let progress = document.querySelectorAll(".skill-progress");
-
-progress.forEach((ele) => {
-  ele.style.width = ele.dataset.width;
-});
-
-// Select Skills Selector 
-let ourSkills = document.querySelector(".skills");
-
-window.onscroll = function () {
-
-// Skills Offset Top 
-let skillsOffsetTop = ourSkills.offsetTop;
-
-//Skills Outer Height 
-let skillsOuterHeight = ourSkills.offsetHeight;
-
-// Window Height
-let windowHeight = this.innerHeight;    
-
-//Window ScrollTop
-let windowScrollTop = this.pageYOffset;     
-
-
-if(windowScrollTop < (skillsOffsetTop + skillsOuterHeight - windowHeight)){
-
-    let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
-    allSkills.forEach(skill =>  {
-
-        skill.style.width = skill.dataset.progress;
-
-    });
 }
-
-};
-
 
